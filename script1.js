7eed56500f467bcc575b229ffa81b8252e2927de
OleksiyKhudoshyn <hudoshin7605@gmail.com>
2024-03-08 11:45:15 +0100

    Script 1 . Anzahl der Datensätze aus der collection mats-workout.userWorkout gruppiert nach            UserID            Workouttype            ,Anzahl der Segmente            created.timestamp.monat            planed by User/Coach (Userid=created.userid)

---
+++ script1.js
@@ -0,0 +1,50 @@
+// Connect to MongoDB
+const {MongoClient} = require('mongodb');
+
+// MongoDB database URL and port
+const url = 'mongodb://localhost:27017';
+const dbName = 'your_database_name';
+
+// Function to execute the query
+async function executeQuery() {
+    const client = new MongoClient(url);
+
+    try {
+        await client.connect();
+        console.log('Successfully connected to the database');
+
+        const db = client.db(dbName);
+
+        // Query for data aggregation
+        const pipeline = [
+            {
+                $group: {
+                    _id: {
+                        userId: '$created.userId',
+                        workoutType: '$workoutType',
+                        month: {$month: '$created.timestamp'},
+                        plannedBy: {
+                            $cond: [
+                                {$eq: ['$userId', '$created.userId']},
+                                'User',
+                                'Coach'
+                            ]
+                        }
+                    },
+                    segmentCount: {$sum: {$size: '$segments'}}
+                }
+            }
+        ];
+
+        const result = await db.collection('mats-workout.userWorkout').aggregate(pipeline).toArray();
+        console.log('Result of the query:', result);
+    } catch (err) {
+        console.error('Error executing the query:', err);
+    } finally {
+        await client.close();
+        console.log('Closed connection to the database');
+    }
+}
+
+// Call the function to execute the query
+executeQuery();
